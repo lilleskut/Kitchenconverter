18f7347f2ae615369ec37192eb299450224feaca
Jens Tobiska <jtobiska@gmail.com>
2015-10-23 21:57:27 +0200

    remove temperature from dimension_array, add area

--- app/src/main/java/com/example/jens/kitchenconverter/MainActivity.java
+++ app/src/main/java/com/example/jens/kitchenconverter/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.jens.kitchenconverter;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
