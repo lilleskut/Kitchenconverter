83f05d4d3db776deb829149c20b16f10ef02a235
Jens Tobiska <jtobiska@gmail.com>
2015-10-26 00:22:05 +0100

    toolbar in settingsactivity

--- app/src/main/java/com/example/jens/kitchenconverter/MyPreferenceFragment.java
+++ app/src/main/java/com/example/jens/kitchenconverter/MyPreferenceFragment.java
@@ -1,8 +1,6 @@
 package com.example.jens.kitchenconverter;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 
@@ -12,43 +10,5 @@ public class MyPreferenceFragment extends PreferenceFragment {
     public void onCreate(final Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
-
-        // Preference units_pref = getPreferenceScreen().findPreference("edit_units");
-        // Preference densities_pref = getPreferenceScreen().findPreference("edit_densities");
-
-        // final Intent units_intent= new Intent(getActivity(),UnitsActivity.class);
-
-/*
-        if(isAdded()) { //otherwise there can be NPE when using getActivity()
-            DataBaseHelper myDbHelper = new DataBaseHelper(getActivity(), getActivity().getFilesDir().getAbsolutePath());
-
-            List<Unit> units = myDbHelper.getAllUnits();
-            List<Density> densities =myDbHelper.getAllDensities();
-            myDbHelper.close();
-
-            PreferenceScreen units_screen = (PreferenceScreen) findPreference("edit_units");
-            PreferenceCategory units_cat = new PreferenceCategory(units_screen.getContext());
-            units_cat.setTitle("Edit Units");
-            units_screen.addPreference(units_cat);
-            for (Unit temp : units) {
-                Preference pref = new Preference(units_screen.getContext());
-                pref.setTitle(temp.getUnit());
-                pref.setSummary(temp.getFactor().toString());
-                units_cat.addPreference(pref);
-            }
-
-
-            PreferenceScreen densities_screen = (PreferenceScreen) findPreference("edit_densities");
-            PreferenceCategory densities_cat = new PreferenceCategory(densities_screen.getContext());
-            densities_cat.setTitle("Edit Densities");
-            densities_screen.addPreference(densities_cat);
-            for (Density temp : densities) {
-                Preference pref = new Preference(densities_screen.getContext());
-                pref.setTitle(temp.getSubstance());
-                pref.setSummary(temp.getDensity().toString());
-                densities_cat.addPreference(pref);
-            }
-        }
-        */
     }
 }
--- app/src/main/java/com/example/jens/kitchenconverter/SettingsActivity.java
+++ app/src/main/java/com/example/jens/kitchenconverter/SettingsActivity.java
@@ -5,13 +5,20 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame, new MyPreferenceFragment()).commit();
     }
 }
