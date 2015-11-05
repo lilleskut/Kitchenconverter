0668765b94dde52ec2b31f4f1898d345b6a37418
Jens Tobiska <jtobiska@gmail.com>
2015-11-05 23:41:17 +0100

    update Density class and database file

--- app/src/main/java/com/example/jens/kitchenconverter/Density.java
+++ app/src/main/java/com/example/jens/kitchenconverter/Density.java
@@ -6,37 +6,32 @@ import android.content.Context;
 public class Density {
 
     private int id;
-    private String substance; // name of substance
+    private int substanceId; // id of substance
     private Double density; // density in kg/l (water = 1)
 
     private final Context ctx;
 
     public Density(Context context) { this.ctx=context; }
-
-    public Density(String substance, Double density, Context context) {
+    public Density(int substanceId, Double density, Context context) {
         super();
         this.ctx = context;
-        setSubstance(substance);
+        setSubstanceId(substanceId);
         setDensity(density);
     }
 
+    // setters
     public void setId(int i) { this.id = i; }
-
-    public void setSubstance(String u) {
-        if( u == null ) {
-            throw new IllegalArgumentException("Substance name is null");
-        }
-        this.substance = u;
+    public void setSubstanceId(int u) {
+        this.substanceId = u;
     }
-
     public void setDensity(Double f) {
         if( f == null ) {
             throw new IllegalArgumentException("Density is null");
         }
         this.density = f; }
 
+    // getters
     public int getId() { return id; }
-    public String getSubstance() { return substance; }
-
+    public int getSubstanceId() { return substanceId; }
     public Double getDensity() { return density; }
 }
